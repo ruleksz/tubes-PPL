a3557695ba733966aec709d7f35949f3c5486a3d
nopal-upel <novalzchri@gmail.com>
2025-12-11 00:32:18 +0700

    [ADD] ROUTES

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Api\CurhatController;
+use App\Http\Controllers\Api\CurhatMessageController;
+
+Route::post('/curhat', [CurhatController::class,'store']);             // buat curhat baru
+Route::get('/curhat', [CurhatController::class,'index']);             // list curhat (public)
+Route::get('/curhat/{id}', [CurhatController::class,'show']);        // detail curhat + messages
+
+Route::post('/curhat/{id}/message', [CurhatMessageController::class,'send']); // user balas
+// admin reply bisa pakai middleware admin (tidak dipakai di contoh sederhana)
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
